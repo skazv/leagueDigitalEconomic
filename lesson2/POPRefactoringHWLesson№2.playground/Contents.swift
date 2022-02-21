ac1da8fe99a68bbe9966205d6cee66127cbac915
skazv <skazv@yandex.ru>
2022-02-21 23:53:14 +0300

    Создал новую структуру Statistic

--- lesson2/POPRefactoringHWLesson№2.playground/Contents.swift
+++ lesson2/POPRefactoringHWLesson№2.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Darwin
 
 // MARK: - ЗАДАНИЕ 1
 // Перепишите из OOP->POP
@@ -37,32 +38,12 @@ protocol Ultimate {
 }
 
 protocol Stats {
-    typealias Statistics = (armor: Float, attackPower: Float, strength: Float, agility: Float, intelligence: Float, hitPoints: Float, mana: Float)
-    
-    var armor: Float? { get set }
-    var attackPower: Float? { get set }
-    var strength: Float? { get set }
-    var agility: Float? { get set }
-    var intelligence: Float? { get set }
-    var hitPoints: Float? { get set }
-    var mana: Float? { get set }
-    
-    mutating func updateStatistics(_ statistics: Statistics)
+    var stat: Statistic { get set }
     
     func info()
 }
 
 extension Stats {
-    mutating func updateStatistics(_ statistics: Statistics) {
-        self.armor = statistics.armor
-        self.attackPower = statistics.attackPower
-        self.strength = statistics.strength
-        self.agility = statistics.agility
-        self.intelligence = statistics.intelligence
-        self.hitPoints = statistics.hitPoints
-        self.mana = statistics.mana
-    }
-    
     func info() {
         print("\(String(describing: self))")
     }
@@ -73,7 +54,16 @@ enum AttakStyle: String {
     case longRange = "long range attak!"
 }
 
-struct Archmage: Movable, Attackable, Stats, Ultimate {
+struct Statistic {
+    typealias Statistics = (armor: Float, attackPower: Float, strength: Float, agility: Float, intelligence: Float, hitPoints: Float, mana: Float)
+    
+    var armor: Float?
+    var attackPower: Float?
+    var strength: Float?
+    var agility: Float?
+    var intelligence: Float?
+    var hitPoints: Float?
+    var mana: Float?
     
     init(statistics: Statistics) {
         self.armor = statistics.armor
@@ -85,14 +75,19 @@ struct Archmage: Movable, Attackable, Stats, Ultimate {
         self.mana = statistics.mana
     }
     
-    var armor: Float?
-    var attackPower: Float?
-    var strength: Float?
-    var agility: Float?
-    var intelligence: Float?
-    var hitPoints: Float?
-    var mana: Float?
-    var haveUltimate: Bool?
+    mutating func updateStatistics(_ statistics: Statistics) {
+        self.armor = statistics.armor
+        self.attackPower = statistics.attackPower
+        self.strength = statistics.strength
+        self.agility = statistics.agility
+        self.intelligence = statistics.intelligence
+        self.hitPoints = statistics.hitPoints
+        self.mana = statistics.mana
+    }
+}
+
+struct Archmage: Movable, Attackable, Stats, Ultimate {
+    var stat: Statistic
     
     func attack() {
         print(AttakStyle.longRange.rawValue)
@@ -104,24 +99,7 @@ struct Archmage: Movable, Attackable, Stats, Ultimate {
 }
 
 struct MountainKing: Movable, Attackable, Ultimate, Stats {
-    
-    init(statistics: Statistics) {
-        self.armor = statistics.armor
-        self.attackPower = statistics.attackPower
-        self.strength = statistics.strength
-        self.agility = statistics.agility
-        self.intelligence = statistics.intelligence
-        self.hitPoints = statistics.hitPoints
-        self.mana = statistics.mana
-    }
-    
-    var armor: Float?
-    var attackPower: Float?
-    var strength: Float?
-    var agility: Float?
-    var intelligence: Float?
-    var hitPoints: Float?
-    var mana: Float?
+    var stat: Statistic
     
     func attack() {
         print(AttakStyle.melee.rawValue)
@@ -133,24 +111,7 @@ struct MountainKing: Movable, Attackable, Ultimate, Stats {
 }
 
 struct AntiMage: Movable, Attackable, Ultimate, Stats {
-    
-    init(statistics: Statistics) {
-        self.armor = statistics.armor
-        self.attackPower = statistics.attackPower
-        self.strength = statistics.strength
-        self.agility = statistics.agility
-        self.intelligence = statistics.intelligence
-        self.hitPoints = statistics.hitPoints
-        self.mana = statistics.mana
-    }
-    
-    var armor: Float?
-    var attackPower: Float?
-    var strength: Float?
-    var agility: Float?
-    var intelligence: Float?
-    var hitPoints: Float?
-    var mana: Float?
+    var stat: Statistic
     
     func attack() {
         print(AttakStyle.melee.rawValue)
@@ -161,14 +122,9 @@ struct AntiMage: Movable, Attackable, Ultimate, Stats {
     
 }
 
-var statistic = Stats.Statistics(armor: 12.0, attackPower: 33.0, strength: 14.0, agility: 44.0, intelligence: 22.0, hitPoints: 850, mana: 285)
-
-var antimage = AntiMage(statistics: statistic)
+var statistic = Statistic(statistics: (armor: 12.0, attackPower: 33.0, strength: 14.0, agility: 44.0, intelligence: 22.0, hitPoints: 850, mana: 285))
+var antimage = AntiMage(stat: statistic)
 
 antimage.attack()
 antimage.info()
 antimage.haveUltimate
-
-
-
-
