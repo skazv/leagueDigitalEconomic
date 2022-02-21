43f7afa590da5841047217232d111c7f50a6bd52
skazv <skazv@yandex.ru>
2022-02-21 21:32:19 +0300

    Рефакторинг

--- lesson2/POPRefactoringHWLesson№2.playground/Contents.swift
+++ lesson2/POPRefactoringHWLesson№2.playground/Contents.swift
@@ -4,23 +4,68 @@ import UIKit
 // Перепишите из OOP->POP
 // Попробуйте использовать Traint/Mexin
 
-protocol AllianceProtocol {
-    typealias Statistics = (armor: Float, attackPower: Float, strength: Float, agility: Float, intelligence: Float, hitPoints: Float, mana: Float)
-
-    var armor: Float? {get set}
-    var attackPower: Float? {get set}
-    var strength: Float? {get set}
-    var agility: Float? {get set}
-    var intelligence: Float? {get set}
-    var hitPoints: Float? {get set}
-    var mana: Float? {get set}
-
-    func attack()
+protocol Movable {
     func move()
     func stop()
-    mutating func updateStatistics(_ statistics: Statistics)
+}
+
+extension Movable {
+    func move() {
+        print("move")
+    }
+    
+    func stop() {
+        print("stop")
+    }
+}
+
+protocol Attackable {
+    func attack()
+    var haveUltimate: Bool { get }
+}
+
+extension Attackable {
+    func attack() {
+        print("attack")
+    }
+    
+    var haveUltimate: Bool { self is Ultimate }
+}
+
+protocol Ultimate {
     func ultimate()
+}
+
+protocol Stats {
+    typealias Statistics = (armor: Float, attackPower: Float, strength: Float, agility: Float, intelligence: Float, hitPoints: Float, mana: Float)
+    
+    var armor: Float? { get set }
+    var attackPower: Float? { get set }
+    var strength: Float? { get set }
+    var agility: Float? { get set }
+    var intelligence: Float? { get set }
+    var hitPoints: Float? { get set }
+    var mana: Float? { get set }
+    
+    mutating func updateStatistics(_ statistics: Statistics)
+    
     func info()
+}
+
+extension Stats {
+    mutating func updateStatistics(_ statistics: Statistics) {
+        self.armor = statistics.armor
+        self.attackPower = statistics.attackPower
+        self.strength = statistics.strength
+        self.agility = statistics.agility
+        self.intelligence = statistics.intelligence
+        self.hitPoints = statistics.hitPoints
+        self.mana = statistics.mana
+    }
+    
+    func info() {
+        print("\(String(describing: self))")
+    }
 }
 
 enum AttakStyle: String {
@@ -28,37 +73,8 @@ enum AttakStyle: String {
     case longRange = "long range attak!"
 }
 
-extension AllianceProtocol {
-
-     mutating func updateStatistics(_ statistics: Statistics) {
-            self.armor = statistics.armor
-            self.attackPower = statistics.attackPower
-            self.strength = statistics.strength
-            self.agility = statistics.agility
-            self.intelligence = statistics.intelligence
-            self.hitPoints = statistics.hitPoints
-            self.mana = statistics.mana
-        }
+struct Archmage: Movable, Attackable, Stats, Ultimate {
     
-    func info() {
-           print("\(String(describing: self))")
-       }
-
-    func attack() {
-        print("attack")
-    }
-
-    func move() {
-        print("move")
-    }
-
-    func stop() {
-        print("stop")
-    }
-}
-
-struct Archmage: AllianceProtocol {
-
     init(statistics: Statistics) {
         self.armor = statistics.armor
         self.attackPower = statistics.attackPower
@@ -68,7 +84,7 @@ struct Archmage: AllianceProtocol {
         self.hitPoints = statistics.hitPoints
         self.mana = statistics.mana
     }
-
+    
     var armor: Float?
     var attackPower: Float?
     var strength: Float?
@@ -76,17 +92,29 @@ struct Archmage: AllianceProtocol {
     var intelligence: Float?
     var hitPoints: Float?
     var mana: Float?
-
+    var haveUltimate: Bool?
+    
     func attack() {
         print(AttakStyle.longRange.rawValue)
     }
-
+    
     func ultimate() {
         print("Avatar")
     }
 }
 
-struct MountainKing: AllianceProtocol {
+struct MountainKing: Movable, Attackable, Ultimate, Stats {
+    
+    init(statistics: Statistics) {
+        self.armor = statistics.armor
+        self.attackPower = statistics.attackPower
+        self.strength = statistics.strength
+        self.agility = statistics.agility
+        self.intelligence = statistics.intelligence
+        self.hitPoints = statistics.hitPoints
+        self.mana = statistics.mana
+    }
+    
     var armor: Float?
     var attackPower: Float?
     var strength: Float?
@@ -98,17 +126,49 @@ struct MountainKing: AllianceProtocol {
     func attack() {
         print(AttakStyle.melee.rawValue)
     }
-
+    
     func ultimate() {
         print("Mass Teleport")
     }
 }
 
-var x = AllianceProtocol.Statistics(armor: 31.0, attackPower: 3.0, strength: 14.0, agility: 17.0, intelligence: 19.0, hitPoints: 450, mana: 285)
-var archimag = Archmage(statistics: x)
+struct AntiMage: Movable, Attackable, Ultimate, Stats {
+    
+    init(statistics: Statistics) {
+        self.armor = statistics.armor
+        self.attackPower = statistics.attackPower
+        self.strength = statistics.strength
+        self.agility = statistics.agility
+        self.intelligence = statistics.intelligence
+        self.hitPoints = statistics.hitPoints
+        self.mana = statistics.mana
+    }
+    
+    var armor: Float?
+    var attackPower: Float?
+    var strength: Float?
+    var agility: Float?
+    var intelligence: Float?
+    var hitPoints: Float?
+    var mana: Float?
+    
+    func attack() {
+        print(AttakStyle.melee.rawValue)
+    }
+    func ultimate() {
+        print("Mana void")
+    }
+    
+}
 
-archimag.info()
-archimag.attack()
+var statistic = Stats.Statistics(armor: 12.0, attackPower: 33.0, strength: 14.0, agility: 44.0, intelligence: 22.0, hitPoints: 850, mana: 285)
+
+var antimage = AntiMage(statistics: statistic)
+
+antimage.attack()
+antimage.info()
+antimage.haveUltimate
+
 
 
 
